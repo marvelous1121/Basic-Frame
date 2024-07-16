7ec84c65214e66ca733b7f9e6dd631afcd8c15bb
marvelous1121 <marvelousmarvy5@gmail.com>
2024-07-16 21:10:47 +0200

    Add files via upload

---
+++ BasicFrame/src/za/ac/tut/ui/FirstFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author marve
+ */
+public class FirstFrame extends JFrame{
+    public FirstFrame(){
+    
+        setTitle("My first GUI");
+        setSize(200, 200);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.red);
+        setVisible(true);
+        
+    }
+}
---
+++ BasicFrameApp/src/basicframeapp/BasicFrameApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.FirstFrame;
+
+
+/**
+ *
+ * @author marve
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declare
+        FirstFrame myframe;
+        
+        //instantiate
+        myframe = new FirstFrame();
+    }
+    
+}
